d1873ea5be81f24d059ad60231f9d7994d999d99
cindy200 <cindyasipa0302@gmail.com>
2021-10-23 08:57:39 +0700

    php mvc sederhana

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -9,14 +9,78 @@ class Barang extends CI_Controller{
         $this->load->model('Barang_model');
     }
 
-    funtion index()
+public  funtion index()
     {
-        $data['judul']="Toko Online";
+        $data['judul']="Barang";
         $data['barang']=$this->Barang_model->getAllBarang();
+        if( $this->input->post('keyword')){
+            $data['barang']=$this->Barang_model->cariDataBarang();
+        }
         $this->load->view('templates/header',$data);
         $this->load->view('barang/index');
         $this->load->view('templates/footer');
        
+    }
+
+public funtion tambah()
+{
+    $data['judul']="Tanbah Data Barang";
+
+    $this->form_validation->set_rules('id_barang', 'Kode Barang', 'required',);
+    $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required',);
+    $this->form_validation->set_rules('harga', 'Harga', 'required|numeric',);
+    $this->form_validation->set_rules('stok', 'Stok', 'required|numeric',);
+    if ($this->form_validation->run() == FALSE)
+             {
+                $this->load->view('templates/header',$data);
+                $this->load->view('barang/tambah');
+                $this->load->view('templates/footer');
+             }
+             else
+             {
+                 $this->Barang_model->tambahDataBarang();
+                 $this->session->set_flashdata('flash','Ditambahkan');
+                 redirect('barang');
+             }
+
+    }
+
+    public function hapus($id){
+        $this->Barang_model->hapusDataBarang($id);
+        $this->session->set_flashdata('flash','Dihapus');
+        redirect('barang');
+
+    }
+    public function detail($id){
+        $data['judul']="Detail Barang";
+        $data['barang']=$this->Barang_model->getBarangById($id);
+        $this->load->view('templates/header',$data);
+        $this->load->view('barang/detail',$data);
+        $this->load->view('templates/footer');
+    
+    }
+
+    public function ubah($id){
+        $data['judul']="Ubah Data Barang";
+        $data['barang']=$this->Barang_model->getBarangById($id);
+        $this->form_validation->set_rules('id_barang', 'Kode Barang', 'required',);
+        $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required',);
+        $this->form_validation->set_rules('harga', 'Harga', 'required|numeric',);
+        $this->form_validation->set_rules('stok', 'Stok', 'required|numeric',);
+        if ($this->form_validation->run() == FALSE)
+                 {
+                    $this->load->view('templates/header',$data);
+                    $this->load->view('barang/ubah',$data);
+                    $this->load->view('templates/footer');
+                 }
+                 else
+                 {
+                     $this->Barang_model->ubahDataBarang();
+                     $this->session->set_flashdata('flash','Diubah');
+                     redirect('barang');
+                 }   
+
+    
     }
 
 }
--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,4 +1,35 @@
 <div class="container">
+<?php if ($this->session->flashdata('flash')); ?>
+<div class="alert alert-succsess alert-dismissible fade show" role="alert">
+  <strong>Data Barang</strong> <?= $this->session->flashdata('flash'); ?>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+<?php endif; ?>
+
+<?php if (empty($barang)):?>
+  <dif class="alert alert-danger" role="alert">
+    Data Barang Tidak ditemukan...
+  <?php endif; ?>
+
+<div class="row-mt-3">
+  <div class="colmd-6">
+    <a href="<?= base_url(); ?>barang/tambah" class="btn btn-primary"> Tambah Data Barang</a>
+  </div>
+</div>
+
+<div class="row mt-3">
+  <div class="col-md-6">
+  <form action="" method="post">
+  <div class="input-group">
+    <input type="text" class="form-control" placeholder="Cari Data Barang..." name="keyword"> 
+    <button class="btn btn-primary" type="submit" >Cari</button>
+</div>
+   </form>
+
+
+  </div>
+</div>
+
 <table class="table">
   <thead>
     <tr>
@@ -6,6 +37,7 @@
       <th scope="col">Nama barang</th>
       <th scope="col">Harga</th>
       <th scope="col">Stok</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -15,7 +47,14 @@
       <td><?= $brg['nama_barang']; ?></td>
       <td><?= $brg['harga']; ?></td>
       <td><?= $brg['stok']; ?></td>
-      </tr>
+      <td> <a href="<?= base_url(); ?>barang/detail/<?= $brg['id_barang']; ?>" class="btn btn-primary"> 
+      >Detail</a> 
+      <a href="<?= base_url(); ?>barang/ubah/<?= $brg['id_barang']; ?>" class="btn btn-success"> 
+      >Ubah</a> 
+      <a href="<?= base_url(); ?>barang/hapus/<?= $brg['id_barang']; ?>" class="btn btn-danger"> 
+      onclick= "return confirm ('Yakin Akan Dihapus?');">Hapus</a> 
+   
+    </tr>
       <?php endforeach ?>
     
   </tbody>
